94538bea4c99de9799c6e5feef13e1697480e2fd
RajSingh8293 <92074370+RajSingh8293@users.noreply.github.com>
2024-10-02 18:07:40 +0530

    Update stripe.routes.js
    add stripe route

--- backend/routes/stripe.routes.js
+++ backend/routes/stripe.routes.js
@@ -1,10 +1,10 @@
 import express, { Router } from "express";
 import { isAuthenticated } from "../middleware/auth.middleware.js";
-import { paymentProcessController } from "../controllers/stripe.controllers.js";
+import { paymentProcessController, stripePublicKey } from "../controllers/stripe.controllers.js";
 
 const router = express.Router();
 
 router.post("/payment/process", isAuthenticated, paymentProcessController);
-// router.get("/stripeapikey", secretStripeKey);
+router.get("/stripepublickey", isAuthenticated, stripePublicKey);
 
 export default router;
