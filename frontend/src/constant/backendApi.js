b3dd75ad16650bf140a9dbbf6684790cc778344e
RajSingh8293 <92074370+RajSingh8293@users.noreply.github.com>
2024-10-02 17:05:53 +0530

    Update backendApi.js

--- frontend/src/constant/backendApi.js
+++ frontend/src/constant/backendApi.js
@@ -1,2 +1,2 @@
-// export const backendApi = "https://ecommerce-web-backend-5o37.onrender.com";
-export const backendApi = "http://localhost:7676";
+export const backendApi = "https://ecommerce-web-backend-5o37.onrender.com";
+// export const backendApi = "http://localhost:7676";
