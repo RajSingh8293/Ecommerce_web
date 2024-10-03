b7bccb52d3f4e3d4a4632b86710a0d7e0c72d585
RajSingh8293 <92074370+RajSingh8293@users.noreply.github.com>
2024-10-03 17:34:06 +0900

    Update SingleProduct.jsx

--- frontend/src/pages/user/SingleProduct.jsx
+++ frontend/src/pages/user/SingleProduct.jsx
@@ -191,8 +191,8 @@ const SingleProduct = () => {
 
                                 </div>
                                 <p className="leading-relaxed">{product.description}</p>
-                                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-                                    <div className="lg:flex items-center">
+                                <div className="lg:flex md:flex flex flex-col gap-3 mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                                    <div className="flex items-center">
                                         <span className="mr-3">Color</span>
                                         <div className="flex gap-3">
                                             {["Red", "Blue", "Black", "Yellow"].map((data, i) =>
